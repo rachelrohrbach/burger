90a3029093584103bdd6e83895a1a0ae83915b34
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-24 14:50:12 -0800

    created burger database and burger table and loaded it with some starter data

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+
+VALUES 
+("cheeseburger", false),
+("mushroom burger", false),
+("turkey burger", false),
+("Big Mac", true),
+("Garden Burger", false),
+("Bacon Swiss", true);
+
+SELECT * FROM burgers;
