18fa59333af55575e081c332af57dbd3b47dbdb8
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-26 15:05:17 -0800

    wrote methods to call ORM functions with burger inputs

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const orm = require('../config/orm.js');
+
+const burger = {
+  selectAll: function(cb) {
+    orm.selctAll('burgers', function(res) {
+      cb(res);
+    });
+  },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
